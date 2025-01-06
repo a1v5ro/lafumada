b7a0b7af04a032ab910df5b188000bf4b7592c11
a1v5ro <163579359+a1v5ro@users.noreply.github.com>
2025-01-06 01:20:06 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+function nextImage(sliderId) {
+    const slider = document.getElementById(sliderId);
+    const group = slider.querySelector('.slider-group');
+    const images = group.children;
+    const imageWidth = images[0].getBoundingClientRect().width; // Incluye márgenes
+    group.style.transition = 'transform 0.5s ease-in-out';
+    group.style.transform = `translateX(-${imageWidth}px)`;
+    setTimeout(() => {
+        group.style.transition = 'none';
+        group.style.transform = 'translateX(0)';
+        group.appendChild(images[0]);
+    }, 500); // Tiempo igual al de la transición
+}
+
+function prevImage(sliderId) {
+    const slider = document.getElementById(sliderId);
+    const group = slider.querySelector('.slider-group');
+    const images = group.children;
+    const imageWidth = images[0].getBoundingClientRect().width; // Incluye márgenes
+    group.style.transition = 'none';
+    group.style.transform = `translateX(-${imageWidth}px)`;
+    group.insertBefore(images[images.length - 1], images[0]);
+    setTimeout(() => {
+        group.style.transition = 'transform 0.5s ease-in-out';
+        group.style.transform = 'translateX(0)';
+    }, 10);
+}
+
+function autoSlide(sliderId) {
+    setInterval(() => nextImage(sliderId), 5000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    autoSlide('cateringSlider');
+    autoSlide('alquilerSlider');
+});
+
+// Cambiar opacidad del header al hacer scroll
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const opacity = Math.max(0.5, 1 - window.scrollY / 300); // Calcula opacidad
+    header.style.backgroundColor = `rgba(51, 51, 51, ${opacity})`; // Ajusta el color del fondo
+});
